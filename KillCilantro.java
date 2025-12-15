1c63dfb87502cd5da3bc2cdfa12a0fdc12e8a2b2
elsadeuxpancera <elsa.deuxpancera@gmail.com>
2025-12-15 09:01:49 -0800

    Update KillCilantro.java

--- KillCilantro.java
+++ KillCilantro.java
@@ -2,17 +2,18 @@ public class KillCilantro {
    public static String killCilantro(String food) {
       String newFood = "";
       for(int i = 0; i < food.length(); i ++) {
-         if(food.contains("cilantro")) {
-            
-           
+         if(i + 8 <= food.length() && food.substring(i, i+8).equals("cilantro")) {           
            newFood += "parsley";
+           i += 7;
+         } else {
+            newFood += food.charAt(i);
          }
       }
       return newFood;
    
    }
    public static void main(String[] args) {
-      System.out.println(killCilantro("cilantro is good"));
+      System.out.println(killCilantro("I dont like cilantro"));
    }
   
 }
